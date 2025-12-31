b968e7a6ff4ec398926794158d14f2dc6106134a
hassa <hassa@10.2.0.2>
2026-01-01 01:27:37 +0300

    Add Supplier class with fields, constructor, getters/setters, and printInfo method

--- OOPInventoryManagementSystem/src/System/Supplier.java
+++ OOPInventoryManagementSystem/src/System/Supplier.java
@@ -2,4 +2,42 @@ package System;
 
 public class Supplier {
 
+    
+    private String name;
+    private String phone;
+    private String email;
+
+    // Constructor
+    public Supplier(String name, String phone, String email) {
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+    }
+
+ 
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+   
+    public void printInfo() {
+        System.out.println("Supplier: " + name + ", Phone: " + phone + ", Email: " + email);
+    }
 }
