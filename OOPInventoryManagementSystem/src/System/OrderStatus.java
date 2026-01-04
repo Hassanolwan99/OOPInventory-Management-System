d23c4903d48627f2d8742d8015f01dfcef43db7c
hassa <hassa@10.2.0.2>
2026-01-05 02:12:36 +0300

    Clean up OrderStatus enum

---
+++ OOPInventoryManagementSystem/src/System/OrderStatus.java
@@ -0,0 +1,10 @@
+package System;
+
+public enum OrderStatus {
+    NEW,        // Order created, editable
+    PAID,       // Payment completed
+    SHIPPED,    // Sent to customer
+    DELIVERED,  // Received by customer
+    COMPLETED,  // Order closed
+    CANCELLED   // Order cancelled
+}
